e619a8d55613f76ebf0ff8be84be9f1ca221ed3e
Thivindu Paranayapa <paranayapathivindu@gmail.com>
2021-08-04 20:03:40 +0530

    adding port

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/' , (req, res) => {res.send("It is working!")});
+app.get('/' , (req, res) => {res.send('It is working!')});
 
 app.post('/signIn' , (req, res) => {signIn.handleSignIn(req, res, db, bcrypt)});
 
